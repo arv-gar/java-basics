3c04159306933059ac9e69e1fc3bf37bcbcfd5dc
arv-gar <67837579+arv-gar@users.noreply.github.com>
2020-07-31 18:13:01 -0400

    Add files via upload
    Fixed the conversion formulas and system in syntax.

--- arvjav.java
+++ arvjav.java
@@ -9,18 +9,18 @@ public class arvjav {
         double userTemp = myScanner.nextDouble();
         System.out.print("Please enter whether the temperature given was in \"F\" or \"C\":");
         String tempType = myScanner.next();
-        myScanner.close();
-
-        if(tempType == "F") {
-            double newTemp = ((5/9)*(userTemp - 32));
+        
+        if(tempType.equals("F")) {
+            double newTemp = (userTemp-32)*5/9;
             System.out.println(newTemp);
         }
-        else if(tempType == "F") {
-            double newTemp = ((userTemp + 32)/(5/9));
+        else if(tempType.equals("C")) {
+            double newTemp = (userTemp*9)/5+32;
             System.out.println(newTemp);
         }
         else {
             System.out.println("Invalid temperature type!");
         }
+        myScanner.close();
     }
 }
